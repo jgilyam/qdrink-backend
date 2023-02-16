56f03db77ed477ea95430ade7ea278a6aafe8b38
jgilyam <jgilyam@folcode.com>
2023-02-16 02:31:35 -0300

    fix

--- src/models/server.ts
+++ src/models/server.ts
@@ -11,7 +11,7 @@ class Server {
 
   constructor() {
     this.app = express();
-    this.port = process.env.PORT || "8000";
+    this.port = process.env.SERVER_PORT || "8000";
 
     this.dbConnection();
     this.middlewares();
