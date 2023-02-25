a8eeedb44c38bd91ce9c2c8cec58ef3b5b8b6b93
jgilyam <jgilyam@folcode.com>
2023-02-25 01:44:18 -0300

    se crea la capa domain

---
+++ src/domain/repositories/CustomerRepository.ts
@@ -0,0 +1,8 @@
+import { CustomerEntity } from "../entities/CustomerEntity";
+
+export interface CustomerRepositry {
+  saveCustomer(customer: CustomerEntity): Promise<CustomerEntity | null>;
+  findAllCustomers(): Promise<CustomerEntity[] | null>;
+  findCustomerById(customerId: number): Promise<CustomerEntity | null>;
+  deleteCustomerById(customerId: number): Promise<CustomerEntity | null>;
+}
