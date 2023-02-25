45b137f89fd69bfb7171a0e2a9666798de933484
jgilyam <jgilyam@folcode.com>
2023-02-25 18:20:44 -0300

    se inyectan los objetos nuevos

--- src/infra/routes/CustomerRoute.ts
+++ src/infra/routes/CustomerRoute.ts
@@ -3,13 +3,22 @@ import { CurstomerService } from "../../aplication/CustomerService";
 import { CustomerController } from "../controllers/CustomerController";
 import dbClient from "../clients/db";
 import { CustomerRepository } from "../repositories/CustomerRepository";
+import { MessagerClient } from "../clients/apiWhatsappBussines/MessagerClient";
+import { MessageMapper } from "../clients/apiWhatsappBussines/domain/mappers/MessageMapper";
 
 const router = Router();
 
 const customerRepository = new CustomerRepository(dbClient);
-const customerService = new CurstomerService(customerRepository);
+
+const messageMapper = new MessageMapper();
+const messagerClient = new MessagerClient(messageMapper);
+const customerService = new CurstomerService(
+  customerRepository,
+  messagerClient
+);
 const customerController = new CustomerController(customerService);
 
 router.get("/", customerController.findAllCustomers);
+router.get("/:id/sendMessage", customerController.sendMessage);
 
 export default router;
