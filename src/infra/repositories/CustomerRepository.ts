85bad9caf5a40da7608d7a864f2cee2e34e533b3
jgilyam <jgilyam@folcode.com>
2023-02-27 07:27:48 -0300

    se implmente endpoitn para buscar cliente

--- src/infra/repositories/CustomerRepository.ts
+++ src/infra/repositories/CustomerRepository.ts
@@ -1,4 +1,22 @@
+import { DataSource, Entity, Repository } from "typeorm";
 import { CustomerEntity2 } from "../../domain/entities";
+import { IPersistence } from "../../domain/interfaces/IPersistence";
 import { BaseRepository } from "./BaseRepository";
 
-export class CustomerRepository extends BaseRepository<CustomerEntity2> {}
+export class CustomerRepository implements IPersistence<CustomerEntity2> {
+  constructor(private readonly repository: Repository<CustomerEntity2>) {}
+  async save(entity: CustomerEntity2): Promise<CustomerEntity2 | null> {
+    return await this.repository.create(entity);
+  }
+  async findAll(): Promise<CustomerEntity2[] | null> {
+    const customers = await this.repository.find();
+    return customers;
+  }
+  async findById(id: number): Promise<CustomerEntity2 | null> {
+    const customer = await this.repository.findOneBy({ id: id });
+    return customer;
+  }
+  async deleteById(id: number): Promise<CustomerEntity2 | null> {
+    throw new Error("Method not implemented.");
+  }
+}
--- src/infra/routes/CustomerRoute.ts
+++ src/infra/routes/CustomerRoute.ts
@@ -1,16 +1,16 @@
 import { Router } from "express";
 import { CurstomerService } from "../../aplication/CustomerService";
 import { CustomerController } from "../controllers/CustomerController";
-import dbClient from "../clients/db";
 import { CustomerRepository } from "../repositories/CustomerRepository";
 import { MessagerClient } from "../clients/apiWhatsappBussines/MessagerClient";
 import { MessageMapper } from "../clients/apiWhatsappBussines/domain/mappers/MessageMapper";
 import { CustomerMapper } from "../../domain/mappers/CustomerMapper";
-
+import { db } from "../db/data-source";
+import { CustomerEntity2 } from "../../domain/entities";
 const router = Router();
 
-const customerRepository = new CustomerRepository(dbClient);
-
+const crepository = db.getRepository(CustomerEntity2);
+const customerRepository = new CustomerRepository(crepository);
 const messageMapper = new MessageMapper();
 const messagerClient = new MessagerClient(messageMapper);
 const customerMapper = new CustomerMapper();
@@ -24,5 +24,6 @@ const customerController = new CustomerController(customerService);
 router.get("/", customerController.findAllCustomers);
 router.get("/:id/sendMessage", customerController.sendMessage);
 router.post("/", customerController.addCustomer);
+router.get("/:id", customerController.findCustomerById);
 
 export default router;
