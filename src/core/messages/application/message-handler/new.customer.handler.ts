cc1dc3802a2d3417091f5b31ff9577ccd44c1f92
jgilyam <jgilyam@folcode.com>
2023-07-17 00:12:05 -0300

    agregando logica el registro de un usuario

--- src/core/messages/application/message-handler/new.customer.handler.ts
+++ src/core/messages/application/message-handler/new.customer.handler.ts
@@ -4,25 +4,28 @@ import { MessageHandler } from "./message.handler";
 
 import { CustomerService } from "../../../customer/application/customer.service";
 import { CustomerInDTO } from "../../../customer/domain";
+import { IMessager } from "../../domain/dtos/message.messager";
 
 export class NewCostumerHandlerCreator extends Creator {
     
     private phone: string
-    constructor(private readonly customerService: CustomerService, phone: string){
+    constructor(private readonly customerService: CustomerService, private readonly messager: IMessager, phone: string){
         super();
         this.phone = phone;
     }
     public createMessageHandler(): MessageHandler {
-        return new NewCostumerHandler(this.customerService, this.phone);
+        return new NewCostumerHandler(this.customerService,this.messager,  this.phone);
     }
 }
 
 class NewCostumerHandler implements MessageHandler {
     
-    private phone: string
+    private phone: string;
+    private qrImage: string
     
-    constructor(private readonly customerService: CustomerService, phone: string){
+    constructor(private readonly customerService: CustomerService, private readonly messager: IMessager, phone: string){
         this.phone = phone;
+        this.qrImage = "";
     }
     
     execute(): void {
@@ -30,10 +33,14 @@ class NewCostumerHandler implements MessageHandler {
         const customerInDTO: CustomerInDTO = {
             phone: this.phone
         };
-        
+
         this.customerService.add(customerInDTO); 
+        this.qrImage = "Estoy deberia ser una imagen codificada en base64"
     }
     reply(): string {
-        return "Hola bienvenido a Qdrink, para cargar saldo, envianos el monto en pesos de la catidad de dinero que queres acreditar en tu cuenta, con ello generaremos una solicitud de pago para que puedas pagar";
+        const response = "Hola bienvenido a Qdrink, para cargar saldo, envianos el monto en pesos de la catidad de dinero que queres acreditar en tu cuenta, con ello generaremos una solicitud de pago para que puedas pagar";
+        
+        this.messager.sendMessageAndImage(this.phone, response, this.qrImage);
+        return response;
     }
 }
