0d673e5be9ee8dc197e5c96b330069486664f562
jgilyam <jgilyam@folcode.com>
2023-07-17 00:12:41 -0300

    agregando logica para cambiar de mensajero

--- src/core/messages/message.dependencies.ts
+++ src/core/messages/message.dependencies.ts
@@ -1,6 +1,17 @@
 import { MessageService } from "./application/message.service";
 import { MessageController } from "./infrastructure/http/message.controller";
+import { MockMessager } from "./infrastructure/mock-messager/mock.messager";
+import { apiWhatsappEnabled } from "./infrastructure/whatsapp-web-js/connection";
+import { WhatsappWebMessager } from "./infrastructure/whatsapp-web-js/whatsapp.web.messager";
+import { customerService } from "../customer/dependencies";
 
-
-export const messageService = new MessageService()
+const chooseMessager = ()=>{
+    if(apiWhatsappEnabled){
+        return new MockMessager();
+    }else{
+        return new WhatsappWebMessager();
+    }
+}
+const messager = chooseMessager(); 
+export const messageService = new MessageService(customerService, messager)
 export const messageController = new MessageController(messageService);
