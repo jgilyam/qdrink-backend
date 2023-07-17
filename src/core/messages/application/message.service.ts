fb4dfcf3a5336691a927a0ccac07fdaca443f44f
jgilyam <jgilyam@folcode.com>
2023-07-17 00:12:29 -0300

    agregando dependecias

--- src/core/messages/application/message.service.ts
+++ src/core/messages/application/message.service.ts
@@ -1,7 +1,6 @@
 import { MessageinDTO } from "../domain/dtos/message.in.dto";
 import { NewCostumerHandlerCreator } from "./message-handler/new.customer.handler";
 import { LoadCreditHandlerCreator } from "./message-handler/load.credit.handler";
-import { Creator } from "./message-handler/message.handler.creator";
 import { RequestQrCodeHandlerCreator } from "./message-handler/request.qrcode.handler";
 import { CustomerService } from "../../customer/application/customer.service";
 import { IMessager } from "../domain/dtos/message.messager";
@@ -13,7 +12,7 @@ export class MessageService {
     receiver = async (body: MessageinDTO)=>{
         const { text, phone } = body;
         
-        const response = this.choseHandler(text, phone);
+        const response = await this.choseHandler(text, phone);
         
         const res = response.run()
     
@@ -22,12 +21,15 @@ export class MessageService {
       
     };
 
-    private choseHandler = (text: string, phone: string) => {
+    private choseHandler = async (text: string, phone: string) => {
       
-      const customer = this.customerService.findByPhone(phone);
-      
+      const customer = await this.customerService.findByPhone(phone);
+      console.log({
+        customer,
+        phone
+      })
       if(!customer) 
-        return new NewCostumerHandlerCreator(this.customerService, phone);
+        return new NewCostumerHandlerCreator(this.customerService, this.messager, phone);
       
         switch (text) {
         case "qr":
