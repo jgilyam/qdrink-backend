4aa1ad20b176f766c947b95a7c41d503d0311e46
jgilyam <jgilyam@folcode.com>
2023-07-17 23:50:34 -0300

    definiendo interfaz de comunicacion con la mensajeria

--- src/core/messages/domain/dtos/message.messager.ts
+++ src/core/messages/domain/dtos/message.messager.ts
@@ -1,4 +1,4 @@
 export interface IMessager {
-    sendMessage(phone: string, text: string) : void;
-    sendMessageAndImage(phone: string, text: string, image: string): void;
+    sendMessage(phone: string, text: string) : Promise<void>;
+    sendMessageAndImage(phone: string, text: string, image: string): Promise<void>;
 }
