5b0730820d34e018d57f2818a10a1a4129253528
jgilyam <jgilyam@folcode.com>
2023-07-23 21:15:51 -0300

    se corrige interfaz

--- src/core/payment/domain/payment.strategy.ts
+++ src/core/payment/domain/payment.strategy.ts
@@ -1,5 +1,5 @@
 import { PaymentRequestAddDto } from "./dtos/payment.request.add.dto";
 
 export interface IPaymentStrategy{
-    createPaymentRequest(paymentRequest: PaymentRequestAddDto): Promise<void>;
+    createPaymentRequest(paymentRequest: PaymentRequestAddDto, userId: string): Promise<string>;
 }
