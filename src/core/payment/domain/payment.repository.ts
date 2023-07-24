697362d0f1aad38398bbbdd4978dc610864c29a8
jgilyam <jgilyam@folcode.com>
2023-07-24 00:42:46 -0300

    se agregan y se implementan metdos en el repo

--- src/core/payment/domain/payment.repository.ts
+++ src/core/payment/domain/payment.repository.ts
@@ -4,4 +4,6 @@ import { PaymentEntity } from "./payment.entity";
 export interface IPaymentRepository{
     add(paymentAddDTO: PaymentAddDTO): Promise<PaymentEntity | null>
     edit(id: string, paymentAddDTO: PaymentAddDTO): Promise<PaymentEntity | null>;
+    findById(id: string): Promise<PaymentEntity | null>;
+    update(paymentEntity: PaymentEntity): Promise<PaymentEntity | null>;
 }
--- src/core/payment/infrastructure/mongo/mongo.payment.repository.ts
+++ src/core/payment/infrastructure/mongo/mongo.payment.repository.ts
@@ -1,3 +1,22 @@
-export class MongoPaymentRepository{
+import { IPaymentRepository, PaymentAddDTO, PaymentEntity } from "../../domain";
+import { Payment } from "./";
+
+export class MongoPaymentRepository implements IPaymentRepository{
+    findById = async (id: string): Promise<PaymentEntity | null> => {
+        return await Payment.findById(id);
+    }
+    update = async (paymentEntity: PaymentEntity): Promise<PaymentEntity | null> =>   {
+        const paymentEdited = await Payment.findByIdAndUpdate(paymentEntity.id,{...paymentEntity}, {new: true})
+        return paymentEdited;
+    }
+    add = async (paymentAddDTO: PaymentAddDTO): Promise<PaymentEntity | null> => {
+        const paymentEntity = new Payment({
+            ...paymentAddDTO,
+          });
+          return await paymentEntity.save();
+    }
+    edit(id: string, paymentAddDTO: PaymentAddDTO): Promise<PaymentEntity | null> {
+        throw new Error("Method not implemented.");
+    }
     
 }
