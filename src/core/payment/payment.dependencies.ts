a5c2ac9edde6225c68a25cec2493b91f4f3428a0
jgilyam <jgilyam@folcode.com>
2023-07-23 21:14:45 -0300

    se crea controlador

--- src/core/payment/payment.dependencies.ts
+++ src/core/payment/payment.dependencies.ts
@@ -1,3 +1,5 @@
 import { PaymentService } from "./application/payment.service";
+import { PaymentController } from "./infrastructure/http/payment.controller";
 
 export const paymentService = new PaymentService();
+export const paymentController = new PaymentController();
