ad139495c87c087779574d1915135d6a13aa075e
jgilyam <jgilyam@folcode.com>
2023-07-25 19:19:01 -0300

    se actualizan variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,6 @@
 export const config = {
     secretJwt: process.env.SECRET_JWT || " ",
     mercadoPagoAccessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN || " ",
-    host: process.env.HOST || " "
+    host: process.env.HOST || " ",
+    mongoDBConnection: process.env.MONGODB_CNN || ""
 }
