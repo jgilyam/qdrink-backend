011bc68656e00f34964bb9e201998ad1058e6faf
jgilyam <jgilyam@folcode.com>
2023-07-26 07:51:00 -0300

    se implementa servicio de busqeuda

---
+++ src/core/accounting-code/application/accounting.code.service.ts
@@ -0,0 +1,11 @@
+import { AccountingCodeKind, DebitCreditKind, SaleChannel } from "../domain/accounting.code.entity";
+import { IAccountingCodeMapper } from "../domain/accounting.code.mapper";
+import { IAccountingCodeRepository } from "../domain/accounting.code.repository";
+
+export class AccountingCodeService {
+    constructor(private readonly accountingCodeRepository: IAccountingCodeRepository, private readonly accountingCodeMapper: IAccountingCodeMapper){}
+    findAll = async (kind?: DebitCreditKind, saleChannel?: SaleChannel, accountingCodeKind?: AccountingCodeKind) =>{
+        const accountingCodeEntities = await this.accountingCodeRepository.findAll(kind, saleChannel, accountingCodeKind)
+        return accountingCodeEntities.map(accountingCode=> this.accountingCodeMapper.customerEntityToCustomerOutDTO(accountingCode));
+    }
+}
