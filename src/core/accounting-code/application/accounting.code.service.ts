f678c5d11779bbe01219fccf7b9eaec5aacb84b2
jgilyam <jgilyam@folcode.com>
2023-08-11 17:09:59 -0300

    implementandop metodos faltantes en el modulo

--- src/core/accounting-code/application/accounting.code.service.ts
+++ src/core/accounting-code/application/accounting.code.service.ts
@@ -1,11 +1,24 @@
-import { AccountingCodeKind, DebitCreditKind, SaleChannel } from "../domain/accounting.code.entity";
+import { AccountingCodeEntity, AccountingCodeKind, DebitCreditKind, SaleChannel } from "../domain/accounting.code.entity";
 import { IAccountingCodeMapper } from "../domain/accounting.code.mapper";
 import { IAccountingCodeRepository } from "../domain/accounting.code.repository";
+import { AccountingCodeOutDTO } from "../domain/dtos/accounting.code.out.dto";
 
 export class AccountingCodeService {
     constructor(private readonly accountingCodeRepository: IAccountingCodeRepository, private readonly accountingCodeMapper: IAccountingCodeMapper){}
     findAll = async (kind?: DebitCreditKind, saleChannel?: SaleChannel, accountingCodeKind?: AccountingCodeKind) =>{
         const accountingCodeEntities = await this.accountingCodeRepository.findAll(kind, saleChannel, accountingCodeKind)
         return accountingCodeEntities.map(accountingCode=> this.accountingCodeMapper.customerEntityToAccountingCodeOutDTO(accountingCode));
+    }
+
+    private findAccountingCodeEntityById = async (id: string): Promise<AccountingCodeEntity> =>{
+        const accoutnitngCodeEntity = await this.accountingCodeRepository.findById(id);
+        if(!accoutnitngCodeEntity) throw new Error();
+        
+        return accoutnitngCodeEntity;
+    }
+    
+    findById = async (id: string): Promise<AccountingCodeOutDTO | null> =>{
+        const accountingCodeEntity = await this.findAccountingCodeEntityById(id);    
+        return this.accountingCodeMapper.customerEntityToAccountingCodeOutDTO(accountingCodeEntity);
     }
 }
