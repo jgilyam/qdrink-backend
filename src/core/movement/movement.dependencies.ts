2c3fe98570936e6b2f0e59496741bc08333a238b
jgilyam <jgilyam@folcode.com>
2023-08-11 17:10:51 -0300

    implementando metodo para agregar movimiento al costumer

--- src/core/movement/movement.dependencies.ts
+++ src/core/movement/movement.dependencies.ts
@@ -1,8 +1,9 @@
 import { MovementService } from "./application/movement.service";
 import { MovementMapper } from "./infrastructure/default.movement.mapper";
-import { accountingCodeMapper } from "../accounting-code/accounting.code.dependencies"
+import { accountingCodeMapper, accoutningCodeService } from "../accounting-code/accounting.code.dependencies"
 import { MongoMovementRepository } from "./infrastructure/mongo/mongo.movement.repository";
+import { customerService } from "../customer/dependencies";
 
 export const movementMapper = new MovementMapper(accountingCodeMapper);
 export const movementRepository = new MongoMovementRepository();
-export const movementService = new MovementService(movementRepository, movementMapper);
+export const movementService = new MovementService(movementRepository, movementMapper, customerService, accoutningCodeService);
