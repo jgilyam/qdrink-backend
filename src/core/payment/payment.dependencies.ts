020baf8c70d8dd2defc659e4c6d7fd43efe213ce
jgilyam <jgilyam@folcode.com>
2023-08-12 08:30:35 -0300

    inyectar dependencia

--- src/core/payment/payment.dependencies.ts
+++ src/core/payment/payment.dependencies.ts
@@ -1,8 +1,9 @@
 import { PaymentController } from "./infrastructure/http/payment.controller";
 import { MongoPaymentRepository } from "./infrastructure/mongo/mongo.payment.repository";
 import { PaymentService } from "./application";
+import { movementService } from "../movement/movement.dependencies";
 
 const paymentRepositorty = new MongoPaymentRepository();
 
-export const paymentService = new PaymentService(paymentRepositorty);
+export const paymentService = new PaymentService(paymentRepositorty, movementService);
 export const paymentController = new PaymentController(paymentService);
