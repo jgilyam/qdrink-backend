c81e90cc1a1f9e66e4039fe47b35e608f4efc430
jgilyam <jgilyam@folcode.com>
2023-08-15 01:40:31 -0300

    feat: implementar funcion verify

--- src/common/jwt.utils.ts
+++ src/common/jwt.utils.ts
@@ -1,14 +1,28 @@
 import jwt from "jsonwebtoken"
 import { config } from "../config/config";
 
-interface CustomerPayload{
+interface Payload{
     sub: string | undefined;
     rol: string;
 }
 
-export const signCustomer = async (payload: CustomerPayload)=>{
-    const {secretJwt} = config
 
+export const signCustomer = async (payload: Payload)=>{
+    const {secretJwt} = config
+    // TODO: sacar el data
     const token = jwt.sign({ data: payload }, secretJwt , { expiresIn: '8h' });
     return token;
+}
+
+export const signTap = async (payload: Payload)=>{
+    const {secretJwt} = config
+
+    const token = jwt.sign( payload , secretJwt , { expiresIn: '24h' });
+    return token;
+}
+
+export const verify = async (token: string): Promise<Payload>  =>{
+    const {secretJwt} = config;
+    const decode = jwt.verify(token, secretJwt);
+    return decode as Payload;
 }
