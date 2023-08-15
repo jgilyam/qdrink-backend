426e0b510d1068eb6d7d909d706d6cddbe7251f3
jgilyam <jgilyam@folcode.com>
2023-08-15 01:41:14 -0300

    feat:implementacion de capa de persistencia

--- src/core/tap/infrastructure/mongo/mongo.tap.repository.ts
+++ src/core/tap/infrastructure/mongo/mongo.tap.repository.ts
@@ -5,19 +5,36 @@ import { Tap } from "./mongo.tap.model";
 
 export class MongoTapRepository implements ITapRepository {
     findByUnitNumber = async (unitNumber: number): Promise<TapEntity | null> => {
-        const tap = await Tap.findOne({unitNumber})
+        const tap = await Tap
+        .findOne({unitNumber})        
+        .populate("drinks")
+        ;
+        
         if(!tap) return tap;
         
-        return await tap.populate("drinks");
+        return await tap.populate("salePoint");
     }
-    add = async(tapInDto: TapAddDTO): Promise<TapEntity> => {
+
+    add = async(salePointId: string, tapInDto: TapAddDTO): Promise<TapEntity> => {
         const {drinkIds, unitNumber} = tapInDto
         const tap = new Tap({
             unitNumber,
-            drinks: drinkIds
+            drinks: drinkIds,
+            salePoint: salePointId
         });
         const result = await tap.save();
         return await result.populate("drinks");
+    }
+
+    findById = async (tapId: string): Promise<TapEntity | null> => {
+        const tap = await Tap
+        .findById(tapId)
+        .populate("drinks")
+        .populate("salePoint");
+
+        if(!tap) return tap;
+        
+        return tap;
     }
 
 }
